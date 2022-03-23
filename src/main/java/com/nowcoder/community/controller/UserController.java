aaa543f0e910b3b5b6d48a0426820d990b8990c7
lzltommy0805 <582215962@qq.com>
2022-03-23 13:10:39 +0800

    个人主页（显示收到的赞）

--- src/main/java/com/nowcoder/community/controller/UserController.java
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -2,6 +2,7 @@ package com.nowcoder.community.controller;
 
 import com.nowcoder.community.annotation.LoginRequired;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.LikeService;
 import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
@@ -43,6 +44,9 @@ public class UserController
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private LikeService likeService;
 
     @LoginRequired
     @RequestMapping(value = "/setting", method = RequestMethod.GET)
@@ -134,6 +138,23 @@ public class UserController
             model.addAttribute("error", "密码错误！");
             return "/site/setting";
         }
+    }
+
+    //个人主页
+    @RequestMapping(path = "/profile/{userId}", method = RequestMethod.GET)
+    public String getProfilePage(@PathVariable("userId") int userId, Model model)
+    {
+        User user = userService.findUserById(userId);
+        if(user == null)
+            throw new RuntimeException("该用户不存在");
+
+        //用户
+        model.addAttribute("user", user);
+        //点赞数量
+        int likeCount = likeService.findUserLikeCount(userId);
+        model.addAttribute("likeCount", likeCount);
+
+        return "/site/profile";
     }
 
 }
--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -4,6 +4,7 @@ public class RedisKeyUtil
 {
     private static final String SPLIT = ":";
     public static final String PREFIX_ENTITY_LIKE = "like:entity";
+    public static final String PREFIX_USER_LIKE = "like:user";
 
     //某个实体的赞
     //like:entity:entityType:entityId -> set(userId)
@@ -13,4 +14,10 @@ public class RedisKeyUtil
         return PREFIX_ENTITY_LIKE + entityType + SPLIT + entityId;
     }
 
+    //某个用户的赞
+    //like:user:userId -> int
+    public static String getUserLikeKey(int userId)
+    {
+        return PREFIX_USER_LIKE + SPLIT + userId;
+    }
 }
