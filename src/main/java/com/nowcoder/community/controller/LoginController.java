ce91bcb711bf5f8837ce33404dcc7b88d4b3789f
lzltommy0805 <582215962@qq.com>
2022-03-24 11:50:27 +0800

    重构登录模块 1.使用Redis存储验证码 2.Redis存储登录凭证 3.Redis存储用户信息

--- src/main/java/com/nowcoder/community/controller/LoginController.java
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -4,11 +4,14 @@ import com.google.code.kaptcha.Producer;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.RedisKeyUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -24,6 +27,7 @@ import javax.servlet.http.HttpSession;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class LoginController implements CommunityConstant
@@ -35,6 +39,9 @@ public class LoginController implements CommunityConstant
 
     @Autowired
     private Producer kaptchaProducer;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Value("${server.servlet.context-path}")
     private String contextPath;
@@ -94,14 +101,24 @@ public class LoginController implements CommunityConstant
     }
 
     @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
-    public void getKaptcha(HttpServletResponse response, HttpSession session)
+    public void getKaptcha(HttpServletResponse response/*, HttpSession session*/)
     {
         //生成验证码
         String text = kaptchaProducer.createText();
         BufferedImage image = kaptchaProducer.createImage(text);
 
         //将验证码存入session
-        session.setAttribute("kaptcha", text);
+//        session.setAttribute("kaptcha", text);
+
+        //存到redis，验证码的归属
+        String kapthcaOwner = CommunityUtil.generateUUID();
+        Cookie cookie = new Cookie("kaptchaOwner", kapthcaOwner);
+        cookie.setMaxAge(60);
+        cookie.setPath(contextPath);
+        response.addCookie(cookie);
+        //将验证码存到redis
+        String redisKey = RedisKeyUtil.getKaptchaKey(kapthcaOwner);
+        redisTemplate.opsForValue().set(redisKey, text, 60, TimeUnit.SECONDS);
 
         //将图片输出给浏览器
         response.setContentType("image/png");
@@ -118,10 +135,17 @@ public class LoginController implements CommunityConstant
 
     @RequestMapping(path = "/login", method = RequestMethod.POST)
     public String login(String username, String password, String code, boolean rememberMe,
-                        Model model, HttpSession session, HttpServletResponse response)
+                        Model model, /*HttpSession session, */HttpServletResponse response,
+                        @CookieValue("kaptchaOwner") String kaptchaOwner)
     {
         //检查验证码
-        String kaptcha = (String) session.getAttribute("kaptcha");
+//        String kaptcha = (String) session.getAttribute("kaptcha");
+        String kaptcha = null;
+        if(StringUtils.isNoneBlank(kaptchaOwner))
+        {
+            String redisKey = RedisKeyUtil.getKaptchaKey(kaptchaOwner);
+            kaptcha = (String) redisTemplate.opsForValue().get(redisKey);
+        }
         if(StringUtils.isBlank(kaptcha) || StringUtils.isBlank(code) || !kaptcha.equalsIgnoreCase(code))
         {
             model.addAttribute("codeMsg", "验证码不正确！");
--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -7,6 +7,10 @@ public class RedisKeyUtil
     public static final String PREFIX_USER_LIKE = "like:user";
     public static final String PREFIX_FOLLOWEE = "followee";
     public static final String PREFIX_FOLLOWER = "follower";
+    public static final String PREFIX_KAPTCHA = "kaptcha";
+    public static final String PREFIX_TICKET = "ticket";
+    public static final String PREFIX_USER = "user";
+
 
     //某个实体的赞
     //like:entity:entityType:entityId -> set(userId)
@@ -37,4 +41,23 @@ public class RedisKeyUtil
     {
         return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT + entityId;
     }
+
+    //登录验证码
+    public static String getKaptchaKey(String owner)
+    {
+        return PREFIX_KAPTCHA + SPLIT + owner;
+    }
+
+    //登录的凭证
+    public static String getTicketKey(String ticket)
+    {
+        return PREFIX_TICKET + SPLIT + ticket;
+    }
+
+    //用户
+    public static String getUserKey(int userId)
+    {
+        return PREFIX_USER + SPLIT + userId;
+    }
+
 }
