9695410324c183c2ea9a7d76a662153729e7e189
lzltommy0805 <582215962@qq.com>
2022-04-02 15:59:25 +0800

    置顶、加精、删除

--- src/main/java/com/nowcoder/community/controller/DiscussPostController.java
+++ src/main/java/com/nowcoder/community/controller/DiscussPostController.java
@@ -162,4 +162,61 @@ public class DiscussPostController implements CommunityConstant
 
         return "/site/discuss-detail";
     }
+
+    //置顶
+    @RequestMapping(path = "/top", method = RequestMethod.POST)
+    @ResponseBody
+    public String setTop(int id)
+    {
+        discussPostService.updatetype(id, 1);
+
+        //同步到es
+        //触发发帖事件
+        Event event = new Event()
+                .setTopic(TOPIC_PUBLISH)
+                .setUserId(hostHolder.getUser().getId())
+                .setEntityType(ENTITY_TYPE_POST)
+                .setEntityId(id);
+        eventProducer.fireEvent(event);
+
+        return CommunityUtil.getJSONString(0);
+    }
+
+    //加精
+    @RequestMapping(path = "/wonderful", method = RequestMethod.POST)
+    @ResponseBody
+    public String setWonderful(int id)
+    {
+        discussPostService.updateStatus(id, 1);
+
+        //同步到es
+        //触发发帖事件
+        Event event = new Event()
+                .setTopic(TOPIC_PUBLISH)
+                .setUserId(hostHolder.getUser().getId())
+                .setEntityType(ENTITY_TYPE_POST)
+                .setEntityId(id);
+        eventProducer.fireEvent(event);
+
+        return CommunityUtil.getJSONString(0);
+    }
+
+    //删除
+    @RequestMapping(path = "/delete", method = RequestMethod.POST)
+    @ResponseBody
+    public String setDelete(int id)
+    {
+        discussPostService.updateStatus(id, 2);
+
+        //同步到es
+        //触发删帖事件
+        Event event = new Event()
+                .setTopic(TOPIC_DELETE)
+                .setUserId(hostHolder.getUser().getId())
+                .setEntityType(ENTITY_TYPE_POST)
+                .setEntityId(id);
+        eventProducer.fireEvent(event);
+
+        return CommunityUtil.getJSONString(0);
+    }
 }
--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -50,4 +50,14 @@ public class DiscussPostService {
     {
         return discussPostMapper.updateCommentCount(id, commentCount);
     }
+
+    public int updatetype(int id, int type)
+    {
+        return discussPostMapper.updateType(id, type);
+    }
+
+    public int updateStatus(int id, int status)
+    {
+        return discussPostMapper.updateStatus(id, status);
+    }
 }
